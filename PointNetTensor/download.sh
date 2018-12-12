51584a2027a4b17050381758e4f41a2afa601078
Giang Truong <hagianga21@gmail.com>
2018-12-12 20:33:48 +0800

    Add tensor for semantic

---
+++ PointNetTensor/download.sh
@@ -0,0 +1,3 @@
+wget https://shapenet.cs.stanford.edu/media/indoor3d_sem_seg_hdf5_data.zip
+unzip indoor3d_sem_seg_hdf5_data.zip
+rm indoor3d_sem_seg_hdf5_data.zip
---
+++ PointNetTensor/provider.py
@@ -0,0 +1,30 @@
+import os
+import sys
+import numpy as np
+import h5py
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(BASE_DIR)
+
+def getDataFiles(list_filename):
+    return [line.rstrip() for line in open(list_filename)]
+
+def load_h5(h5_filename):
+    f = h5py.File(h5_filename)
+    data = f['data'][:]
+    label = f['label'][:]
+    return (data, label)
+
+def loadDataFile(filename):
+    return load_h5(filename)
+
+def shuffle_data(data, labels):
+    """ Shuffle data and labels.
+        Input:
+          data: B,N,... numpy array
+          label: B,... numpy array
+        Return:
+          shuffled data, label and shuffle indices
+    """
+    idx = np.arange(len(labels))
+    np.random.shuffle(idx)
+    return data[idx, ...], labels[idx], idx
